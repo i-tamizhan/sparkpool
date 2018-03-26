b6f65cf67143c69c7e8f3f4d3dce86afb1bf6d85
joe <vimaljose4@gmail.com>
2018-03-26 11:30:05 +0530

    First commit

--- src/CoiniumServ/Utils/ConsoleWindow.cs
+++ src/CoiniumServ/Utils/ConsoleWindow.cs
@@ -1,32 +1,3 @@
-﻿#region License
-// 
-//     MIT License
-//
-//     CoiniumServ - Crypto Currency Mining Pool Server Software
-//     Copyright (C) 2013 - 2017, CoiniumServ Project
-//     Hüseyin Uslu, shalafiraistlin at gmail dot com
-//     https://github.com/bonesoul/CoiniumServ
-// 
-//     Permission is hereby granted, free of charge, to any person obtaining a copy
-//     of this software and associated documentation files (the "Software"), to deal
-//     in the Software without restriction, including without limitation the rights
-//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//     copies of the Software, and to permit persons to whom the Software is
-//     furnished to do so, subject to the following conditions:
-//     
-//     The above copyright notice and this permission notice shall be included in all
-//     copies or substantial portions of the Software.
-//     
-//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//     SOFTWARE.
-// 
-#endregion
-
 using System;
 
 namespace CoiniumServ.Utils
@@ -42,12 +13,12 @@ namespace CoiniumServ.Utils
         public static void PrintBanner()
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(@"             .__       .__                                         ");
-            Console.WriteLine(@"  ____  ____ |__| ____ |__|__ __  _____   ______ ______________  __");
-            Console.WriteLine(@"_/ ___\/  _ \|  |/    \|  |  |  \/     \ /  ___// __ \_  __ \  \/ /");
-            Console.WriteLine(@"\  \__(  <_> )  |   |  \  |  |  /  Y Y  \\___ \\  ___/|  | \/\   / ");
-            Console.WriteLine(@" \___  >____/|__|___|  /__|____/|__|_|  /____  >\___  >__|    \_/  ");
-            Console.WriteLine(@"     \/              \/               \/     \/     \/             ");
+			Console.WriteLine(@" ____                   _                      _ ");
+			Console.WriteLine(@"/ ___| _ __   __ _ _ __| | ___ __   ___   ___ | |");
+			Console.WriteLine(@"\___ \| '_ \ / _` | '__| |/ / '_ \ / _ \ / _ \| |");
+			Console.WriteLine(@" ___) | |_) | (_| | |  |   <| |_) | (_) | (_) | |");
+			Console.WriteLine(@"|____/| .__/ \__,_|_|  |_|\_\ .__/ \___/ \___/|_|");
+			Console.WriteLine(@"      |_|                   |_|    ");
             Console.WriteLine();
         }
 
@@ -57,17 +28,7 @@ namespace CoiniumServ.Utils
         public static void PrintLicense()
         {
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("Copyright (C) 2013 - 2018, Coinium project - https://github.com/CoiniumServ/CoiniumServ");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("CoiniumServ comes with ABSOLUTELY NO WARRANTY.");
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("You can contribute the development of the project by donating;");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(" BTC : 1MMdwRZg4K9p6oH2bWUQdohbxGbAvXS1t1");
-            Console.WriteLine(" ETH : 0x61aa3e0709e20bcb4aedc2607d4070f1db72e69b");
-            Console.WriteLine(" LTC : Ld8cy4ucf3FYThtfTnRQFFp5MKK9rZHjNg");
+            Console.WriteLine(" ETH : 0xE095327C3E0d8e92A5870772aBC40163F2BBD956");
             Console.WriteLine();
             Console.ResetColor();
         }
